3cb61efd7ae37e98d3dd33162ba2b000508a5dd0
Alex Dmitrijev <z-alex-z@hotmail.com>
2023-08-03 16:40:17 +0300

    feat: 8888

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import { Button, Header } from "ui";
 export default function Page() {
   return (
     <>
-      <Header text="Web 3" />
+      <Header text="Web 8888" />
       <Button />
     </>
   );
