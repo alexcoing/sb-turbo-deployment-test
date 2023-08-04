460ed8ebcabd98b7d7ace50a9bef6a16f0b603e5
Alex Dmitrijev <z-alex-z@hotmail.com>
2023-08-04 16:32:19 +0300

    update

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -3,7 +3,7 @@ import { Button, Header } from "ui";
 export default function Page() {
   return (
     <>
-      <Header text="Docs 807b" />
+      <Header text="Docs 807bc" />
       <Button />
     </>
   );
