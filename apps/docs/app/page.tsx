fe13af64c78c62ea03df4ae618b391c3eb43fe3f
Alex Dmitrijev <z-alex-z@hotmail.com>
2023-08-04 14:55:14 +0300

    feat: test 500

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -3,7 +3,7 @@ import { Button, Header } from "ui";
 export default function Page() {
   return (
     <>
-      <Header text="Docs 299" />
+      <Header text="Docs 500" />
       <Button />
     </>
   );
